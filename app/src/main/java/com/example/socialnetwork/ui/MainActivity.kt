18e6a3b61b4dcf596424aed81ad8d6a9b0d7793b
anorovhasan <60793393+anorovhasan@users.noreply.github.com>
2020-08-02 09:05:44 +0500

    temporary branch for fixing

--- app/src/main/java/com/example/socialnetwork/ui/MainActivity.kt
+++ app/src/main/java/com/example/socialnetwork/ui/MainActivity.kt
@@ -1,11 +1,13 @@
-package com.example.socialnetwork
+package com.example.socialnetwork.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.socialnetwork.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
